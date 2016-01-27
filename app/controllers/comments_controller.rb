8cc53d049fd4ddede6a5241f9a2d3ed25f561660
John Caruzzo <johncaruzzo@Johns-MacBook-Pro.local>
2016-01-26 21:16:28 -0600

    Kyle's fix for the update method

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,27 +1,37 @@
 class CommentsController < ApplicationController
 #	http_basic_authenticate_with name: "dhh", password: "secret", only: :destroy
 
- def edit
-   @article = Article.find(params[:article_id])
-   @comment = @article.comments.find(params[:id])
-   redirect_to article_path(@comment)
- end
+def edit
+ @article = Article.find(params[:article_id])
+ @comment = @article.comments.find(params[:id])
+end
 
-  def create
-    @article = Article.find(params[:article_id])
-    @comment = @article.comments.create(comment_params)
-    redirect_to article_path(@article)
-  end
- 
- def destroy
-    @article = Article.find(params[:article_id])
-    @comment = @article.comments.find(params[:id])
-    @comment.destroy
-    redirect_to article_path(@article)
-  end
+def create
+  @article = Article.find(params[:article_id])
+  @comment = @article.comments.create(comment_params)
+  redirect_to article_path(@article)
+end
 
-  private
-    def comment_params
-      params.require(:comment).permit(:commenter, :body)
-    end
+def destroy
+  @article = Article.find(params[:article_id])
+  @comment = @article.comments.find(params[:id])
+  @comment.destroy
+  redirect_to article_path(@article)
+end
+
+def update
+  @article = Article.find(params[:article_id])
+  @comment = @article.comments.find(params[:id])
+
+  if @comment.update(comment_params)
+    redirect_to @article
+  else
+    render 'edit'
+  end
+end
+
+private
+def comment_params
+  params.require(:comment).permit(:commenter, :body)
+end
 end
